44680dde228613ec25b2299874323828d1d25127
Нуртилек Кошкар <nurtylek98@gmail.com>
2023-02-21 21:24:33 +0600

    stanalone components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TodoListComponent,
-    TodoItemComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo-list/todo-item/todo-item.component.ts
+++ src/app/todo-list/todo-item/todo-item.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+import {Photo} from "../models/photo";
 
 @Component({
   selector: 'element-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
+  standalone: true
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent implements OnInit, OnDestroy, OnChanges, DoCheck {
+  @Input() photo!: Photo;
+  @Output() onDelete = new EventEmitter<number>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    // const { photo } = changes;
+    //
+    // console.log(photo.currentValue)
+    console.log('ngOnChanges')
   }
 
+  ngOnInit(): void {
+    console.log('INIT APP');
+  }
+
+  ngDoCheck() {
+    console.log('DO CHECK');
+  }
+
+  ngOnDestroy() {
+    console.log('DESTROYED', this.photo.id);
+  }
 }
