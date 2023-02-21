54c2f2dddd71e10f41e37a29be73d4c0086fdcb4
Нуртилек Кошкар <nurtylek98@gmail.com>
2023-02-21 20:41:12 +0600

    comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
